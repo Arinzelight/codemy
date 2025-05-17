8af5add9c21034b7a5a0919aef136151901f9d3c
Arinzelight <arinzelight2@gmail.com>
2025-05-17 21:08:21 +0100

    add assets files and install required packages

--- app/(routes)/onboarding/index.tsx
+++ app/(routes)/onboarding/index.tsx
@@ -1,10 +1,11 @@
+import OnboardingScreen from "@/screens/onboarding/OnboardingScreen";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function index() {
   return (
     <View>
-      <Text>onboarding</Text>
+      <OnboardingScreen />
     </View>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,27 @@
-import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import React, { useEffect, useState } from "react";
 
-export default function index() {
-  const [loggedInUser, setLoggedInUser] = React.useState(true);
-  const [isLoading, setIsLoading] = React.useState(true);
+export default function Index() {
+  const [loggedInUser, setLoggedInUser] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {}, []);
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  );
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await SecureStore.getItemAsync("accessToken");
+        setLoggedInUser(!!token);
+      } catch (error) {
+        console.error("Error accessing SecureStore:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (isLoading) return null;
+
+  return <Redirect href={loggedInUser ? "/" : "/onboarding"} />;
 }
